5dca48d63cbc5898150aa4cfc0b918ae1300b0c5
DantheCE <dan.doyin@gmail.com>
2026-02-23 16:03:56 +0100

    feat: Add blog list utility functions for total likes and favorite blog, along with their unit tests.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,10 +6,27 @@ const totalLikes = (array) => {
   return array[array.length - 1].likes
 }
 
-// const favoriteBlog = (array) => {
-
-// }
+const favoriteBlog = (array) => {
+  let i
+  let mostLikes = 0
+  let mostLikesIndex = 0
+  let presentLikes = 0
+  for (i = 0; i < array.length; i++){
+    if (i === 0) {
+      mostLikes = array[i].likes
+      mostLikesIndex = i
+      continue
+    }
+    presentLikes = array[i].likes
+    if (presentLikes > mostLikes){
+      mostLikes = presentLikes
+      mostLikesIndex = i
+    }
+  }
+  return array[mostLikesIndex]
+}
 module.exports = {
 //   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
