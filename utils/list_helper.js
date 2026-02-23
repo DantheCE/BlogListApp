0b279fe5831dfecc81bc344bbff996cd1bfa7dfe
DantheCE <dan.doyin@gmail.com>
2026-02-23 21:30:47 +0100

    feat: Implement blog list helper functions for calculating total likes, finding the favorite blog, and identifying the author with the most blogs, along with their corresponding unit tests.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -25,8 +25,70 @@ const favoriteBlog = (array) => {
   }
   return array[mostLikesIndex]
 }
+
+const itemCheck = (array, string) => {
+  if (array.length === 0){
+    return false
+  }
+
+  for (let i = 0; i < array.length; i++){
+    if (string === array[i]){
+      return true
+    }
+  }
+  return false
+}
+
+const mostBlogs = (array) => {
+
+  let authors = []
+  let authorsPlain = []
+
+  for (let i = 0; i < array.length; i++){
+    if (itemCheck(authorsPlain, array[i].author)){
+      continue
+    }
+    let obj = {
+      author: array[i].author,
+      numberofPosts: 0
+    }
+    authors.push(obj)
+    authorsPlain.push(array[i].author)
+  }
+
+
+  for (let i = 0; i < authors.length; i++){
+    let authorName = authors[i].author
+    for (let j = 0; j < array.length; j++){
+      if (authorName === array[j].author){
+        authors[i].numberofPosts++
+      }
+    }
+  }
+
+  let maxPostsCurrent = 0
+  let maxPostsIndex = 0
+  let presentNumberPost = 0
+
+  for (let i = 0; i < authors.length; i++){
+    if (i === 0){
+      maxPosts = authors[i].numberofPosts
+      maxPostsIndex = i
+      continue
+    }
+    presentNumberPost = authors[i].numberofPosts
+    if (presentNumberPost > maxPostsCurrent){
+      maxPostsCurrent = presentNumberPost
+      maxPostsIndex = i
+    }
+  }
+
+  return authors[maxPostsIndex]
+}
+
 module.exports = {
 //   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
