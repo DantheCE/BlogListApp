58c4054b9cc6f2d75a924db2f356193499a26ce7
DantheCE <dan.doyin@gmail.com>
2026-02-23 21:51:08 +0100

    feat: implement blog list helper functions including total likes, favorite blog, most blogs, and most likes, along with their unit tests.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -86,9 +86,109 @@ const mostBlogs = (array) => {
   return authors[maxPostsIndex]
 }
 
+const mostLikes = (array) => {
+  let authors = []
+  let authorsPlain = []
+
+  for (let i = 0; i < array.length; i++){
+    if (itemCheck(authorsPlain, array[i].author)){
+      continue
+    }
+    let obj = {
+      author: array[i].author,
+      numberofLikes: 0
+    }
+    authors.push(obj)
+    authorsPlain.push(array[i].author)
+  }
+
+
+  for (let i = 0; i < authors.length; i++){
+    let authorName = authors[i].author
+    for (let j = 0; j < array.length; j++){
+      if (authorName === array[j].author){
+        authors[i].numberofLikes += array[j].likes
+      }
+    }
+  }
+
+  let maxLikesCurrent = 0
+  let maxLikesIndex = 0
+  let presentNumberLikes = 0
+
+  for (let i = 0; i < authors.length; i++){
+    if (i === 0){
+      maxLikesCurrent = authors[i].numberofLikes
+      maxLikesIndex = i
+      continue
+    }
+    presentNumberLikes = authors[i].numberofLikes
+    if (presentNumberLikes > maxLikesCurrent){
+      maxLikesCurrent = presentNumberLikes
+      maxLikesIndex = i
+    }
+    console.log(authors[maxLikesIndex])
+  }
+
+  return authors[maxLikesIndex]
+}
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  mostLikes(listWithMultipleBlogs)
 module.exports = {
 //   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
