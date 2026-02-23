672e92a9c8e5e69b11d51ae5b8accc8c7692a87d
DantheCE <dan.doyin@gmail.com>
2026-02-23 15:44:48 +0100

    feat: Set up testing infrastructure with `node --test` and add the first test for the `totalLikes` utility function.

---
+++ utils/list_helper.js
@@ -0,0 +1,15 @@
+// const dummy = (array) => {
+//   return 1
+// }
+
+const totalLikes = (array) => {
+  return array[array.length - 1].likes
+}
+
+// const favoriteBlog = (array) => {
+
+// }
+module.exports = {
+//   dummy,
+  totalLikes
+}
