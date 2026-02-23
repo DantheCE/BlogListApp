7949962686c7e40f951a197264349edd9c090b5f
DantheCE <dan.doyin@gmail.com>
2026-02-23 10:05:22 +0100

    feat: set up initial blog list backend with API routes, Mongoose model, and utility modules for configuration, logging, and middleware.

--- Controllers/blogListRouter.js
+++ Controllers/blogListRouter.js
@@ -3,18 +3,18 @@ const Blog = require('../models/blogList')
 const logger = require('../utils/logger')
 
 blogListRouter.get('/', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
 })
 
 blogListRouter.post('/', (request, response) => {
-   logger.info('request', request.body) 
-    const blog = new Blog(request.body)
+  logger.info('request', request.body)
+  const blog = new Blog(request.body)
 
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
 })
 
 module.exports = blogListRouter
